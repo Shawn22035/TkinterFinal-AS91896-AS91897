c12a5b5e2f8c9c1a0a246e650348e3b333d3356f
Shawn Lee <102930226+Shawn22035@users.noreply.github.com>
2023-05-15 11:21:06 +1200

    Added pages
    Adding pages that can now store data

--- File.py
+++ File.py
@@ -6,4 +6,27 @@ root.geometry("500x500")
 Simple=Label(text="something just a test or something im not too sure tbh")
 Simple.pack()
 
+def newpage():
+
+    secondpage = Toplevel(root)
+
+    secondpage.geometry("500x500")
+
+    something=Label(secondpage,
+                    text="something").pack()
+
+def threepage():
+
+    thirdpage =Toplevel(root)  
+    thirdpage.geometry("500x500")
+
+    another=Label(thirdpage,
+                  text="something else").pack()
+
+page1 = Button(text="Heres a button for a page",command=newpage)
+page1.pack()
+
+page2 = Button(text="This is a second page",command=threepage)
+page2.pack()
 root.mainloop()
+
