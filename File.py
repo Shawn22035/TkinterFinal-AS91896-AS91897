142dcaea9bf61af0a57ca0964c753e8c5b2f51a5
Shawn Lee <102930226+Shawn22035@users.noreply.github.com>
2023-05-16 10:02:26 +1200

    Get Function
    get entry name

--- File.py
+++ File.py
@@ -3,30 +3,40 @@ from tkinter import *
 root = Tk()
 root.geometry("500x500")
 
-Simple=Label(text="something just a test or something im not too sure tbh")
-Simple.pack()
+Simple=Label(text="something just a test or something im not too sure tbh").pack()
 
 def newpage():
 
     secondpage = Toplevel(root)
-
     secondpage.geometry("500x500")
 
     something=Label(secondpage,
                     text="something").pack()
 
-def threepage():
+    nameinp=Entry(secondpage)
+    nameinp.pack()
 
+    def done():
+        global namepr
+        namepr=nameinp.get()
+        print(namepr)
+
+    nameprint=Button(secondpage,
+                     text="print",
+                     command=done).pack()
+                
+
+def threepage():
     thirdpage =Toplevel(root)  
     thirdpage.geometry("500x500")
 
     another=Label(thirdpage,
                   text="something else").pack()
 
-page1 = Button(text="Heres a button for a page",command=newpage)
-page1.pack()
+page1 = Button(text="Heres a button for a page",command=newpage).pack()
 
-page2 = Button(text="This is a second page",command=threepage)
-page2.pack()
+page2 = Button(text="This is a second page",command=threepage).pack()
+
+
 root.mainloop()
 
