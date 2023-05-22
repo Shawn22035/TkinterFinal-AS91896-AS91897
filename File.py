fe1634af1b56b6a6173f5de1b065694a29817c4f
Shawn Lee <102930226+Shawn22035@users.noreply.github.com>
2023-05-23 11:04:14 +1200

    Error functions
    more formatting, if functions, except functions, error messages

--- File.py
+++ File.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
 root.geometry("500x500")
@@ -13,6 +14,23 @@ def newpage():
     secondpage.resizable(False,False)
     secondpage.title("Julie's Party Hire!")
 
+    def done():
+
+        fullname = namegrab.get()
+
+        if fullname.isalpha() == False:
+            messagebox.showerror("Error", "Please only use numbers in name")
+            
+        try:
+            reciept = int(barcodeinp.get())
+        except: 
+            messagebox.showerror("Error", "Please Input Numbers for Narcode")
+
+        item = choices.get()
+
+        if item=="...":
+            messagebox.showerror("Item Choice", "Please select hired item")       
+
     def page1done():
         secondpage.destroy()
     
@@ -22,16 +40,15 @@ def newpage():
                     font='bold').grid(column=2,row=1)
 
     name=Label(secondpage, 
-                text="Name:").grid(column=1,row=2, padx=60,pady=30, sticky=W)
+                  text="Name:").grid(column=1,row=2, padx=60,pady=30, sticky=W)
 
-    nameinp=Entry(secondpage).grid(column=2,row=2)
+    namegrab=Entry(secondpage).grid(column=1,row=3)
 
-    barcodeinp=Entry(secondpage).grid(column=3,row=3)
-                
-    def done():
-        global namepr
-        namepr=nameinp.get()
-        print(namepr)
+    barcode=Label(secondpage,
+                    text="Barcode No.").grid(column=2,row=2)
+
+    
+    barcodeinp=Entry(secondpage).grid(column=2,row=3)
 
     nameprint=Button(secondpage,
                      text="print",
@@ -40,10 +57,19 @@ def newpage():
     somebutton=Button(secondpage,
                         text="Return!",
                         command=page1done).grid(column=3,row=4)
-
-    invalid=Message("text didnt work")
     
 
+    choices= StringVar()
+    choices.set("...")
+
+    itemhire=Label(secondpage, 
+                    text="Item Hired").grid(column=3,row=2)
+
+    hired = OptionMenu(secondpage, choices,
+                    "...","Spoon","Fork","Plates","Hats","Candles","Confetti").grid(column=3,row=3,padx=30,pady=30)
+
+
+    
 
 def threepage(): 
     thirdpage =Toplevel(root)  
@@ -52,6 +78,7 @@ def threepage():
     another=Label(thirdpage,
                   text="something else").grid(column=1,row=1)
 
+#Buttons that direct to input & output windows
 page = Button(text="Heres a button for a page",command=newpage).grid(column=1,row=2)
 
 page2 = Button(text="This is a second page",command=threepage).grid(column=2,row=2)
