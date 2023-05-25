cf74ff9c29520c8d7666ce9792565c21fa731b89
Shawn Lee <102930226+Shawn22035@users.noreply.github.com>
2023-05-25 15:06:21 +1200

    New Pages

--- File.py
+++ File.py
@@ -5,8 +5,11 @@ root = Tk()
 root.geometry("500x500")
 root.title("Julie's Party Hire!")
 
+var1=something.get()
+
 Simple=Label(text="something just a test or something im not too sure tbh").grid(column=2,row=1)
 
+#Code that opens a new 500x500 input window titled Julie's Party Hire
 def newpage():
 
     secondpage = Toplevel(root)
@@ -14,6 +17,7 @@ def newpage():
     secondpage.resizable(False,False)
     secondpage.title("Julie's Party Hire!")
 
+#Collecting data and outputting error messages
     def done():
 
         fullname = namegrab.get()
@@ -30,11 +34,11 @@ def newpage():
 
         if item=="...":
             messagebox.showerror("Item Choice", "Please select hired item")       
-
+#Closes the input page
     def page1done():
         secondpage.destroy()
     
-
+#Labels and Input boxes
     something=Label(secondpage,
                     text="something",
                     font='bold').grid(column=2,row=1)
@@ -54,11 +58,12 @@ def newpage():
                      text="print",
                      command=done).grid(column=1,row=4) 
     
+    #Button that input window
     somebutton=Button(secondpage,
                         text="Return!",
                         command=page1done).grid(column=3,row=4)
     
-
+#Dropdown box to select what item was hired
     choices= StringVar()
     choices.set("...")
 
@@ -70,7 +75,7 @@ def newpage():
 
 
     
-
+#Variable that opens the output window
 def threepage(): 
     thirdpage =Toplevel(root)  
     thirdpage.geometry("500x500")
@@ -79,6 +84,13 @@ def threepage():
                   text="something else").grid(column=1,row=1)
 
 #Buttons that direct to input & output windows
+
+something = Entry(root).grid(column=1,row=3)
+
+somethingbut=Button(root, command=get)
+
+somethinge = Label(root, text=obj.get("var1")).grid(column=3,row=2)
+
 page = Button(text="Heres a button for a page",command=newpage).grid(column=1,row=2)
 
 page2 = Button(text="This is a second page",command=threepage).grid(column=2,row=2)
